6240c07eb895a2310d61a6e0a1007fa2ff0a7cdd
tobi-oye <tobiloba5000@gmail.com>
2020-08-28 21:39:56 +0100

    finalized version of pixel art maker

--- test.js
+++ test.js
@@ -1,11 +1,13 @@
 let submit = document.querySelector('input[type = "submit"]');
-let button = document.querySelector('button');
+
+
+// function to create tables based on value inside the text field
+function makeGrid(event) {
 let height = document.querySelector('#inputHeight');
 let heightVal = Number(height.value);
 let width = document.querySelector('#inputWidth');
 let widthVal = Number(width.value);
-// function to create tables based on value inside the text field
-function makeGrid() {
+
     for (let i = 0; i < heightVal; i++){
         let row = document.createElement('tr');
 
@@ -16,9 +18,34 @@ function makeGrid() {
         let tableElement = document.querySelector('#pixelCanvas');
         tableElement.appendChild(row);
     }
+    return  event.preventDefault();  
+ 
 }
 
-// button.addEventListener('click',makeGrid);
+   let tableElement = document.querySelector('#pixelCanvas');
+//    tableElement.className
+   tableElement.addEventListener('click', function (evt){
+    // evt.target.className = 'test';
+    // document.querySelector('.test').style.backgroundColor = blue;
+    if (evt.target.style.backgroundColor === ''){
+        evt.target.style.backgroundColor = (document.querySelector('#colorPicker')).value;
+    }
+    else{
+        evt.target.style.backgroundColor = '';
+    }
 
+    
+    // evt.target.classList.toggle('test');
+})
+
+
+// let button = document.querySelector('button');
+// button.addEventListener('click',function(event){
+//     tableElement.innerHTML = "";
+//     return event.preventDefault();
+// });
+
+document.addEventListener('DOMContentLoaded',submit.addEventListener('click',makeGrid));
 // click event listener for submit button that will call the makegrid function  above
-submit.addEventListener('click',makeGrid);
+// submit.addEventListener('click',makeGrid);
+
